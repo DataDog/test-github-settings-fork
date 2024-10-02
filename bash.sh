4ca9180b55a94be6e0edfea98ca082a970264532
Claire Thibodeaux <claire.thibodeaux@datadoghq.com>
2024-10-02 16:35:08 -0400

    delete chmod in fork

--- bash.sh
+++ bash.sh
@@ -3,9 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Make sure the script is executable (this line will not be needed if permissions are set in the GitHub Actions workflow)
-chmod +x ./bash.sh  # This line doesn't typically belong here; it's usually done in the workflow
-
 # Check if the TOKEN environment variable is set
 if [[ -z "$TOKEN" ]]; then
   echo "Error: TOKEN environment variable is not set."
